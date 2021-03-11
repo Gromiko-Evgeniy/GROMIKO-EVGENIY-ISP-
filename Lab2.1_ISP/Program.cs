51afb9c201623f43b5f730d3424b5062d5fbaab8
Gromiko-Evgeniy <74821242+Gromiko-Evgeniy@users.noreply.github.com>
2021-03-11 22:53:05 +0300

    Add files via upload

---
+++ Lab2.1_ISP/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2._1_ISP
+{
+    class Program
+    {
+        static void ZeroCounter(string dateTime)
+        {
+            int numberOfZeros = 0;
+            for (int i = 0; i < dateTime.Length; i++)
+            {
+                if (dateTime[i] == '0')
+                {
+                    numberOfZeros++;
+                }
+            }
+            Console.WriteLine($"\ncurrent date & time: {dateTime}");
+            Console.WriteLine($"number of zeros in this format: {numberOfZeros}");
+        }
+
+        static void Main(string[] args)
+        {
+            string dateTime1 = DateTime.Now.ToString();
+            ZeroCounter(dateTime1);
+
+            string dateTime2 = DateTime.Now.ToString("dd MMMM yyyy HH:mm:ss");
+            ZeroCounter(dateTime2);
+        }
+    }
+}
---
+++ Lab2.1_ISP/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Lab2.1_ISP")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab2.1_ISP")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("818951e5-02d2-42c4-a85c-1ef36e060486")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
