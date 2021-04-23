1fc7ca953de1f6eecabd7ad4f87570afc4920c4f
Gromiko-Evgeniy <74821242+Gromiko-Evgeniy@users.noreply.github.com>
2021-04-23 17:19:52 +0300

    Add files via upload

---
+++ Lab3_ISP/Audi.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_ISP
+{
+    class Audi : Car
+    {
+        public Audi(string _fyelType, float _fyelQuantity, float _fyelConsumption, float _fuelTankVolume, float _maxSpeed, float _accelerationTimeToMaxSpeed,
+        bool _automaticTransmissionType, bool _presenceOfAutopilot, bool _presenceOfConditioner, bool _presenceOfRemoteControl, bool _presenceOfSportCarMode)
+       : base(_fyelType, _fyelQuantity, _fyelConsumption, _fuelTankVolume, _maxSpeed, _accelerationTimeToMaxSpeed)
+        {
+            automaticTransmissionType = _automaticTransmissionType;
+            presenceOfAutopilot = _presenceOfAutopilot;
+            presenceOfConditioner = _presenceOfConditioner;
+            presenceOfRemoteControl = _presenceOfRemoteControl;
+            presenceOfSportCarMode = _presenceOfSportCarMode;
+        }
+        public override uint CarRate() 
+        {
+            uint carRate = 0;
+            carRate += Exhaust();
+            if (presenceOfConditioner)
+            {
+                carRate++;
+            }
+            if (automaticTransmissionType)
+            {
+                carRate++;
+            }
+            if (presenceOfAutopilot)
+            {
+                carRate += 2;
+            }
+            if (presenceOfRemoteControl)
+            {
+                carRate += 2;
+            }
+            if (presenceOfSportCarMode)
+            {
+                carRate += 2;
+            }
+            return carRate;
+        }
+        public override void ShowAllInfo()
+        {
+            Console.WriteLine($"your vehicle type is {vehicleType} \n" +
+            $"{Assist.outputAssistant1} type is {fyelType} \n" +
+            $"{Assist.outputAssistant1} quantity is {fyelQuantity} liters \n" +
+            $"{Assist.outputAssistant1} consumption is {fyelConsumption} liters/100km \n" +
+            $"{Assist.outputAssistant1} tank volume is {fuelTankVolume} liters \n" +
+            $"your max. speed is {maxSpeed} km/h \n" +
+            $"{Assist.outputAssistant2}n automatic transmissionType? it's {automaticTransmissionType} \n" +
+            $"{Assist.outputAssistant2} Conditioner? it's {presenceOfConditioner} \n" +
+            $"{Assist.outputAssistant2}n Autopilot? it's {presenceOfAutopilot} \n" +
+            $"{Assist.outputAssistant2} sport car mode)? it's {presenceOfSportCarMode} \n" +
+            $"{Assist.outputAssistant2} remote control? it's {presenceOfRemoteControl} \n");
+        }
+    }
+}
---
+++ Lab3_ISP/Car.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_ISP
+{
+    abstract class Car : Vehicle
+    {
+        protected struct OutputAssistant
+        {
+            public string outputAssistant1;
+            public string outputAssistant2;
+            public string outputAssistant3;
+        }
+
+        protected OutputAssistant Assist = new OutputAssistant();
+
+        public Car(string _fyelType, float _fyelQuantity, float _fyelConsumption, float _fuelTankVolume, float _maxSpeed, float _accelerationTimeToMaxSpeed)
+            : base("Car", _fyelType, _fyelQuantity, _fyelConsumption, _fuelTankVolume, _maxSpeed, _accelerationTimeToMaxSpeed)
+        {
+            Assist.outputAssistant1 = "your fyel ";
+            Assist.outputAssistant2 = "Is there a";
+            Assist.outputAssistant3 = "Something's wrong";
+        }
+
+        protected bool automaticTransmissionType; // car part
+        protected bool presenceOfAutopilot;
+        protected bool presenceOfConditioner;
+        protected bool presenceOfRemoteControl;
+        protected bool presenceOfSportCarMode;
+
+
+        public enum MotorAge
+        {
+            newMotor = 1,
+            oldMotor
+        }
+
+        public MotorAge motorAge = MotorAge.oldMotor;
+
+        public void ChangeMotor(string newFyelType, float newFyelConsumption, float newMaxSpeed, float newAccelerationTimeToMaxSpeed)
+        {
+            fyelType = newFyelType;
+            fyelConsumption = newFyelConsumption;
+            maxSpeed = newMaxSpeed;
+            accelerationTimeToMaxSpeed = newAccelerationTimeToMaxSpeed;
+            MotorAge motorAge = MotorAge.newMotor;
+        }
+
+        public MotorAge MotorAgeCheck()
+        {
+            return motorAge;
+        }
+
+        public uint Exhaust()
+        {
+            if (fyelType == "Electricity")
+            {
+                return 3;
+            }
+            else if (fyelType == "Gas")
+            {
+                return (uint)(3 - fyelConsumption / 10);
+            }
+            else if (fyelType == "Diesel")
+            {
+                return (uint)(2 - fyelConsumption / 10);
+            }
+            else if (fyelType == "Petrol")
+            {
+                return (uint)(2 - fyelConsumption / 10);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public abstract uint CarRate();
+
+        public virtual void ShowAllInfo()
+        {
+            Console.WriteLine($"your vehicle type is {vehicleType}\n" +
+            $"your fyel type is {fyelType}\n" +
+            $"your fyel quantity is {fyelQuantity} liters \n" +
+            $"your fyel consumption is {fyelConsumption} liters/100km \n" +
+            $"your fyel tank volume is {fuelTankVolume} liters \n" +
+            $"your max. speed is {maxSpeed} km/h\n" +
+            $"your acceleration time to max speed is {accelerationTimeToMaxSpeed} seconds \n");
+        } 
+
+    }
+}
