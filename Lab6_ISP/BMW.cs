7e167bc94b5ee42f92875e9c439b83ad8a7fbb8d
Gromiko-Evgeniy <74821242+Gromiko-Evgeniy@users.noreply.github.com>
2021-04-30 09:35:09 +0300

    Add files via upload

---
+++ Lab6_ISP/BMW.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_ISP
+{
+
+    class BMW : Car, IComparable, IVehicleDocumentation
+    {
+        public BMW (string _ownerName, string _fyelType, float _fyelQuantity, float _fyelConsumption, float _fuelTankVolume, float _maxSpeed, float _accelerationTimeToMaxSpeed,
+            bool _automaticTransmissionType, bool _presenceOfAutopilot, bool _presenceOfConditioner, bool _presenceOfRemoteControl, bool _presenceOfSportCarMode)
+        : base(_ownerName, _fyelType, _fyelQuantity, _fyelConsumption, _fuelTankVolume, _maxSpeed, _accelerationTimeToMaxSpeed) 
+        {
+            automaticTransmissionType = _automaticTransmissionType;
+            presenceOfAutopilot = _presenceOfAutopilot;
+            presenceOfConditioner = _presenceOfConditioner;
+            presenceOfRemoteControl = _presenceOfRemoteControl;
+            presenceOfSportCarMode = _presenceOfSportCarMode;
+            ownerName = _ownerName;
+        }
+
+        public override uint CarRate() 
+        {
+            uint carRate = 0;
+            carRate += Exhaust();
+            if (presenceOfConditioner)
+            {
+                carRate++;
+            }
+            if (automaticTransmissionType)
+            {
+                carRate++;
+            }
+            if (presenceOfAutopilot)
+            {
+                carRate += 2;
+            }
+            if (presenceOfRemoteControl)
+            {
+                carRate += 2;
+            }
+            if (presenceOfSportCarMode)
+            {
+                carRate += 2;
+            }
+            return carRate;
+        }
+
+        public override void ShowAllInfo()
+        {
+            Console.WriteLine($"your vehicle type is {vehicleType} \n" +
+            $"{Assist.outputAssistant1} type is {fyelType} \n" +
+            $"{Assist.outputAssistant1} quantity is {fyelQuantity} liters \n" +
+            $"{Assist.outputAssistant1} consumption is {fyelConsumption} liters/100km \n" +
+            $"{Assist.outputAssistant1} tank volume is {fuelTankVolume} liters \n" +
+            $"your max. speed is {maxSpeed} km/h \n" +
+            $"{Assist.outputAssistant2}n automatic transmissionType? it's {automaticTransmissionType} \n"+
+            $"{Assist.outputAssistant2} Conditioner? it's {presenceOfConditioner} \n"+
+            $"{Assist.outputAssistant2}n Autopilot? it's {presenceOfAutopilot} \n"+
+            $"{Assist.outputAssistant2} sport car mode)? it's {presenceOfSportCarMode} \n"+
+            $"{Assist.outputAssistant2} remote control? it's {presenceOfRemoteControl} \n");
+        }
+        public int CompareTo(object obj)
+        {
+            if (obj is BMW BMWobj)
+            {
+                double mySpeed = MaxSpeed;
+                double itsSpeed = BMWobj.MaxSpeed;
+                if (mySpeed == itsSpeed)
+                {
+                    return 0;
+                }
+                else if (mySpeed < itsSpeed)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+            throw new NotSupportedException();
+        }
+
+        public void ChangeOwner(string newName)
+        {
+            ownerName = newName;
+        }
+        public string GetOwner()
+        {
+            return ownerName;
+        }
+        public void PassInspection(string newInspectionDate)
+        {
+            inspectionDate = newInspectionDate;
+        }
+        public string GetInspectionDate()
+        {
+            return inspectionDate;
+        }
+
+        //static void Documentation(IVehicleDocumentation ObjectDocumentation, string documentationActon, string iformation)
+        //{
+        //    if (documentationActon == "ChangeOwner")
+        //    {
+        //        ObjectDocumentation.ChangeOwner(iformation);
+        //    }
+        //    else
+        //    {
+        //        ObjectDocumentation.PassInspection(iformation);
+        //    }
+        //}
+
+        //static void Documentation(IVehicleDocumentation ObjectDocumentation, string documentationActon)
+        //{
+        //    if (documentationActon == "GetOwner")
+        //    {
+        //        ObjectDocumentation.GetOwner();
+        //    }
+        //    else
+        //    {
+        //        ObjectDocumentation.GetInspectionDate();
+        //    }
+        //}
+    }
+}
---
+++ Lab6_ISP/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_ISP
+{
+    class Program
+    {
+        static void Main()
+        {
+            BMW X5 = new BMW("Vasia", "Petrol", 60, 9, 94 ,230, 14, true, false, true, true, true);
+            BMW X6 = new BMW("Petia", "Petrol", 80, 9, 94 ,250, 10, true, false, true, true, true);
+            Console.WriteLine($"My subjective assessment of the car is {X6.CarRate()}");
+            Console.WriteLine($"You have an {X6.MotorAgeCheck()}");
+            X5.ShowAllInfo();
+
+            int compareIndex = X6.CompareTo(X5);
+            if (compareIndex == 0)
+            {
+                Console.WriteLine("the maximum speed is the same \n" + $"compareIndex = {compareIndex}");
+            }
+            else if (compareIndex == 1)
+            {
+                Console.WriteLine("bmw X6 is faster \n" + $"compareIndex = { compareIndex}");
+            }
+            else
+            {
+                Console.WriteLine("bmw X5 is faster \n" + $"compareIndex = { compareIndex}");
+            }
+
+            X6.ChangeOwner("Kolia");
+            Console.WriteLine(X6.GetOwner());
+            X6.PassInspection("today");
+            Console.WriteLine(X6.GetInspectionDate());
+        }
+    }
+}
